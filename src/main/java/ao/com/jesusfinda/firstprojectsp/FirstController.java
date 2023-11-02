7f22f66ab09ac1d016b3ff2915b2b179e2dfb7f7
JesusFinda <jesusfinda13@gmail.com>
2023-11-03 00:07:55 +0100

    First Controller

---
+++ src/main/java/ao/com/jesusfinda/firstprojectsp/FirstController.java
@@ -0,0 +1,17 @@
+package ao.com.jesusfinda.firstprojectsp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/firstController")
+public class FirstController {
+    @GetMapping("/firstMethods")
+
+    public String firstMethod(){
+        return "Meu primeiro método api rest";
+    }
+
+    
+}
