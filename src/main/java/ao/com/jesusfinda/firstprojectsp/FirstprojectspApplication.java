ded8262535142e969218eb4917964e366a25bdf9
JesusFinda <jesusfinda13@gmail.com>
2023-11-02 23:24:03 +0100

    First Commit

---
+++ src/main/java/ao/com/jesusfinda/firstprojectsp/FirstprojectspApplication.java
@@ -0,0 +1,13 @@
+package ao.com.jesusfinda.firstprojectsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/com/jesusfinda/firstprojectsp/FirstprojectspApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.com.jesusfinda.firstprojectsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
