c476325257ffe660e905aa302d0d448c0ed950b1
JesusFinda <jesusfinda13@gmail.com>
2023-11-03 01:11:21 +0100

    Component Scan

--- src/main/java/ao/com/jesusfinda/firstprojectsp/FirstprojectspApplication.java
+++ src/main/java/ao/com/jesusfinda/firstprojectsp/FirstprojectspApplication.java
@@ -2,8 +2,10 @@ package ao.com.jesusfinda.firstprojectsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ao.com.jesusfinda")
 public class FirstprojectspApplication {
 
 	public static void main(String[] args) {
