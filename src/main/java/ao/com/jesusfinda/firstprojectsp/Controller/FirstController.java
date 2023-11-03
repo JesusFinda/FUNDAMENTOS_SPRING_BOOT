f164834b5cfef5844b334d773c4d7a4cc82a4b6c
JesusFinda <jesusfinda13@gmail.com>
2023-11-03 08:16:41 +0100

    body params

--- src/main/java/ao/com/jesusfinda/firstprojectsp/Controller/FirstController.java
+++ src/main/java/ao/com/jesusfinda/firstprojectsp/Controller/FirstController.java
@@ -1,17 +1,24 @@
 package ao.com.jesusfinda.firstprojectsp.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/firstController")
 public class FirstController {
-    @GetMapping("/firstMethods")
 
-    public String firstMethod(){
-        return "Meu primeiro método api rest";
+    @GetMapping("/firstMethods/{id}")
+    public String firstMethod(@PathVariable String id){
+        return "O parametro é: "+id;
     }
 
+    @PostMapping("/methodWithBodyParams")
+    public String metthodWithBodyParams(@RequestBody String username){
+        return "metthodWithBodyParams "+ username;
+    }
     
 }
